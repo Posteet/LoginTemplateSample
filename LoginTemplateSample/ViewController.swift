e4bfc418335336b3f881dd8c51f85f0da8f296af
Thomas <acadia3000@gmail.com>
2016-02-25 13:56:22 +0900

    Initial Commit

---
+++ LoginTemplateSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginTemplateSample
+//
+//  Created by Thomas on 2016. 2. 25..
+//  Copyright © 2016년 Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
